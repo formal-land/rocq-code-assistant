25d878e11479746c56117ed5c9cae6f6ffbd1a38
andreadlm <delmastro2000@gmail.com>
2025-01-30 17:35:21 +0100

    Added test

--- src/test/syntaxes/comments.test.v
+++ src/test/syntaxes/comments.test.v
@@ -20,3 +20,7 @@ Qed.
      forall A B : Prop, A -> B -> A /\ B. *)
 //   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ - meta.proof.coq
 // <-------------------------------------------- comment.block.coq - meta.proof.coq
+
+(* Lemma and_intro *) : forall A B : Prop, A -> B -> A /\ B.
+// <--------------------- comment.block.coq - meta.proof.coq
+//                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ - comment.block.coq meta.proof.coq
