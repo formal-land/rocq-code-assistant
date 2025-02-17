0f9d0a00c0748c38d98b5048d1dfc1f594ccb3d1
andreadlm <delmastro2000@gmail.com>
2025-02-17 09:59:54 +0100

    Experimental extension main

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,12 @@
 import * as vscode from 'vscode';
-import * as lsp from 'vscode-languageserver-types';
 import { LanguageClient, VersionedTextDocumentIdentifier } from 'vscode-languageclient/node';
 import * as utils from './utils';
 import * as tokenizer from './syntax/tokenizer';
 import * as cpqLSP from './coq-lsp-client';
 import * as ollamaModelProvider from './model-providers/ollama';
 import * as extractors from './syntax/extractors';
-import { GoalRequest } from './lib/coq-lsp/types';
 import * as basicLLM from './oracles/basic-LLM/basic-LLM';
+import { search } from './search';
 
 let coqLSPClient: LanguageClient | undefined;
 
@@ -48,42 +47,25 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(regHelloWorld);
 
   const regSolve = vscode.commands.registerCommand('rocq-coding-assistant.solve', async () => {
+    if (!coqLSPClient) return Promise.reject('Coq LSP client not started');
+
     const editor = vscode.window.activeTextEditor;
-    
     if (!editor) return;
 
     const text = editor.document.getText();
     const tokenizedText = await coqTokenizer.tokenize(text, 'source.coq.proof');
     const splittedText = text.split(/\r?\n|\r|\n/g);
 
-    const proof = extractors.extractProofAtPosition(editor.selection.active, splittedText, tokenizedText);
+    const proof = extractors.extractProofAtPosition(editor.document.uri.toString(), editor.selection.active, splittedText, tokenizedText);
     if (proof === undefined) { 
       vscode.window.showErrorMessage('Not in a theorem'); 
       return; 
     }
 
-    const goals = await Promise.all(proof.admitsLocations.map(async (admitsLocation) => {
-      const params: GoalRequest = {
-        textDocument: VersionedTextDocumentIdentifier.create(editor.document.uri.toString(), editor.document.version),
-        position: lsp.Position.create(admitsLocation.start.line, admitsLocation.start.character),
-        pp_format: 'Str'
-      };
-
-      const goal = await coqLSPClient?.sendRequest(cpqLSP.goalReq, params);
-
-      return goal;
-    }));
-
-    const [model] = await vscode.lm.selectChatModels({vendor: 'ollama'});
-
+    const [model] = await vscode.lm.selectChatModels({ vendor: 'ollama' });
     const oracle = basicLLM.create(model);
 
-    const goal = goals[0]?.goals?.given_up[0];
-
-    if (goal !== undefined) {
-      const response = await oracle.query(goal);
-      console.log(response);
-    }
+    search(proof, coqLSPClient, [oracle]);
   });
   context.subscriptions.push(regSolve);
 }
--- src/search.ts
+++ src/search.ts
@@ -1,84 +1,14 @@
 import { LanguageClient } from 'vscode-languageclient/node';
-import { Proof, ProofMeta } from './syntax/coq-proof';
-import { Stack } from './utils';
+import { ProofMeta } from './syntax/proof';
+import { Oracle } from './oracles/oracle';
+import { PetState } from './lib/coq-lsp/types';
 import { Request } from './coq-lsp-client';
-import { GoalConfig, PpString } from './lib/coq-lsp/types';
 
-type Element = Block | string;
-
-class Block {
-  elements: Element[];
-  lastChecked: number;
-
-  constructor() {
-    this.elements = [];
-    this.lastChecked = -1;
-  }
-
-  nextToBeChecked() {
-    if (this.lastChecked < this.elements.length - 1)
-      return this.elements[++this.lastChecked];
-  }
-
-  proof() : Proof {
-    return this.elements
-      .map(element => element instanceof Block ? element.proof() : [element])
-      .reduce((proof, element) => proof.concat(element));
-  }
+export async function search(proof: ProofMeta, coqLSPClient: LanguageClient, oracles: Oracle[]) {
+  const sState = await coqLSPClient.sendRequest(Request.Petanque.start, { uri: proof.uri, thm: proof.name, pre_commands: null });
+  _search(proof, coqLSPClient, sState, oracles);
 }
 
-export class ProofTemplate {
-  mainBlock: Block;
-  blockStack: Stack<Block>;
-  state: { st: number, proof_finished: boolean };
-  coqLSPClient: LanguageClient;
-
-  private constructor(state: { st: number, proof_finished: boolean }, coqLSPClient: LanguageClient, 
-    startingElements?: Element[]) {
-    this.mainBlock = new Block();
-    this.blockStack = new Stack();
-    this.state = state;
-    this.coqLSPClient = coqLSPClient;
-
-    if (startingElements) 
-      this.mainBlock.elements.push(...startingElements);
-
-    this.blockStack.push(this.mainBlock);
-  }
-
-  update(element: Element) {
-    this.blockStack.peek()?.elements.push(element);
-  }
-
-  async checkUpdates() {
-    let element: Element | undefined;
-    let goals: GoalConfig<PpString>;
-
-    while (element = this.blockStack.peek()?.nextToBeChecked()) 
-      if (element instanceof Block) {
-        this.blockStack.push(element);
-      } else {
-        this.state = await this.coqLSPClient.sendRequest(
-          Request.Petanque.run, { st: this.state.st, tac: element });
-        goals = await this.coqLSPClient.sendRequest(
-          Request.Petanque.goals, { st: this.state.st });
-        if (goals.goals.length === 0)
-          this.blockStack.pop();
-      }
-  }
-
-  proof() {
-    return this.mainBlock.proof();
-  }
-
-  static async fromProofMeta(proofMeta: ProofMeta, coqLSPClient: LanguageClient) {
-    const startState = await coqLSPClient.sendRequest(
-      Request.Petanque.start, { uri: proofMeta.uri, thm: proofMeta.name, pre_commands: null });
-
-    const startingElements = proofMeta.body.map(({ token, tags }) =>
-      tags.includes('meta.proof.body.tactic.admit.coq') ? new Block() : token
-    );
-
-    return new ProofTemplate(startState, coqLSPClient, startingElements);
-  }
+async function _search(proof: ProofMeta, coqLSPClient: LanguageClient, sState: PetState, oracles: Oracle[]) {
+  const { status, message, goal, state } = await proof.check(coqLSPClient, sState);
 }
