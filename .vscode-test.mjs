de5ba6a8453c47982a37ae62a3ecb27c2102ef14
andreadlm <delmastro2000@gmail.com>
2025-02-20 11:13:08 +0100

    Better benchmark template

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -5,7 +5,7 @@ export default defineConfig(
     label: 'miniF2F',
     files: 'out/test/benchmarks/miniF2F/miniF2F.test.js',
     launchArgs: ['--enable-proposed-api=formal-land.rocq-coding-assistant'],
-    workspaceFolder: './src/test/benchmarks/miniF2F/dataset',
+    workspaceFolder: './src/test/benchmarks/miniF2F',
   }, {
     label: 'extension',
     files: 'out/test/extension.test.js',
--- src/test/benchmarks/miniF2F/miniF2F.test.ts
+++ src/test/benchmarks/miniF2F/miniF2F.test.ts
@@ -2,13 +2,26 @@ import * as assert from 'assert';
 import * as YAML from 'yaml';
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import { Commands } from '../../../extension';
 
 interface Test {
   file: string,
   theorems: string[]
 }
 
-const DATASET_DESCRIPTION_PATH = './miniF2F.yaml';
+const DATASET_DESCRIPTION_PATH = './dataset/miniF2F.yaml';
+
+async function _testTheorem(file: string, proofName: string) {
+  if (!vscode.workspace.workspaceFolders) {
+    assert.fail();
+  } else {
+    const fileUri = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, file);
+    const fileDocument = await vscode.workspace.openTextDocument(fileUri);
+    vscode.window.showTextDocument(fileDocument);
+    const ret = await vscode.commands.executeCommand(Commands.HELLO_WORLD, proofName);
+    assert.strictEqual(ret, 0);
+  }
+}
 
 suite('miniF2F benchmark', () => {
   let dsDescription: Test[] = [];
@@ -22,9 +35,6 @@ suite('miniF2F benchmark', () => {
   dsDescription
     .flatMap(({ file, theorems }) =>
       theorems.map(theorem => ({ file: file, theorem: theorem })))
-    .forEach(({ file, theorem }) => {
-      test(`Test ${file}:${theorem}`, () => {
-        assert.strictEqual(1, 1);
-      });
-    });
+    .forEach(({ file, theorem }) => 
+      test(`Test ${file}:${theorem}`, () => _testTheorem(file, theorem)));
 });
