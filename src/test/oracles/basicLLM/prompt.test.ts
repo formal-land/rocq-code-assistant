d04b7e969b4d54ff712eb6500383136ecd22be95
andreadlm <delmastro2000@gmail.com>
2025-03-20 15:30:16 +0100

    Added more tests

--- src/test/oracles/basicLLM/prompt.test.ts
+++ src/test/oracles/basicLLM/prompt.test.ts
@@ -4,7 +4,7 @@ import * as utils from '../../../utils';
 import * as prompt from '../../../oracles/basic-LLM/prompt';
 
 suite('Extension Test Suite', () => {
-  test('Prompt 1', async () => {
+  test('Complete', () => {
     const goal = {
       ty: 'test x && forallb test l = true',
       hyps: [
@@ -40,6 +40,46 @@ Unable to unify "true" with "test x && forallb test l".
     };
 
     const messages = prompt.render(goal, params);
+
+    assert.fail('\n' + utils.languageModelChatMessagesToString(messages));
+  });
+
+  test('Without error history', () => {
+    const goal = {
+      ty: 'test x && forallb test l = true',
+      hyps: [
+        { ty: 'Type', names: ['X'] },
+        { ty: 'X -> bool', names: ['test'] },
+        { ty: 'X', names: ['x', 'y'] },
+        { ty: 'list X', names: ['l'] },
+        { ty: 'forallb test l = true <-> All (fun x0 : X => test x0 = true) l', names: ['IHl'] },
+        { ty: 'test x = true /\\ All (fun x0 : X => test x0 = true) l', names: ['H'] }
+      ]
+    };
+
+    const params = {
+      errorHistory: []
+    };
+
+    const messages = prompt.render(goal, params);
+
+    assert.fail('\n' + utils.languageModelChatMessagesToString(messages));
+  });
+
+  test('Without parameters', () => {
+    const goal = {
+      ty: 'test x && forallb test l = true',
+      hyps: [
+        { ty: 'Type', names: ['X'] },
+        { ty: 'X -> bool', names: ['test'] },
+        { ty: 'X', names: ['x', 'y'] },
+        { ty: 'list X', names: ['l'] },
+        { ty: 'forallb test l = true <-> All (fun x0 : X => test x0 = true) l', names: ['IHl'] },
+        { ty: 'test x = true /\\ All (fun x0 : X => test x0 = true) l', names: ['H'] }
+      ]
+    };
+
+    const messages = prompt.render(goal);
 
     assert.fail('\n' + utils.languageModelChatMessagesToString(messages));
   });
