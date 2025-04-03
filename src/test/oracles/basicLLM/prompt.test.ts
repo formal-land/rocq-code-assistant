3b8c9fca6915af4d8e29c0ff578e09252f58d140
andreadlm <delmastro2000@gmail.com>
2025-04-03 16:12:22 +0200

    Added another test case for goals without hypoteses

--- src/test/oracles/basicLLM/prompt.test.ts
+++ src/test/oracles/basicLLM/prompt.test.ts
@@ -52,6 +52,47 @@ Unable to unify "true" with "test x && forallb test l".
     assert.fail('\n' + utils.languageModelChatMessagesToString(messages));
   });
 
+  test('Without hypoteses', () => {
+    const goal = {
+      ty: 'test x && forallb test l = true',
+      hyps: []
+    };
+
+    const params = {
+      errorHistory: [{
+        tactics: [
+          Token.Standard.FOCUSING_CONSTRUCT_TACTIC('tactic.'),
+          Token.Standard.FOCUSING_CONSTRUCT_DASH,
+          Token.Standard.FOCUSING_CONSTRUCT_TACTIC('tactic1; tactic2.'),
+          Token.Standard.FOCUSING_CONSTRUCT_CROSS,
+          Token.Standard.FOCUSING_CONSTRUCT_TACTIC('tactic.'),
+          Token.Standard.FOCUSING_CONSTRUCT_CROSS,
+          Token.Standard.FOCUSING_CONSTRUCT_TACTIC('tactic.'),
+          Token.Standard.FOCUSING_CONSTRUCT_TACTIC('tactic.'),
+          Token.Standard.FOCUSING_CONSTRUCT_DASH,
+          Token.Standard.FOCUSING_CONSTRUCT_TACTIC('tactic.')
+        ],
+        at: 2,
+        message:
+`
+Coq: In environment
+X : Type
+test : X -> bool
+x : X
+l : list X
+IHl : forallb test l = true <-> All (fun x : X => test x = true) l
+Hx : test x = true
+Hl : All (fun x : X => test x = true) l
+Unable to unify "true" with "test x && forallb test l".
+`
+      }]
+    };
+
+    const messages = prompt.render(goal, params);
+
+    assert.fail('\n' + utils.languageModelChatMessagesToString(messages));
+  });
+
   test('Without error history', () => {
     const goal = {
       ty: 'test x && forallb test l = true',
