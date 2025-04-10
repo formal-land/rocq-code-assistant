fca649efee9e2637b63a4d112e60f95bc6f9205d
andreadlm <delmastro2000@gmail.com>
2025-04-10 16:11:29 +0200

    Grammar for hints in comments

--- src/test/syntaxes/coq-proof/comments.test.v
+++ src/test/syntaxes/coq-proof/comments.test.v
@@ -24,3 +24,22 @@ Qed.
 (* Lemma and_intro *) : forall A B : Prop, A -> B -> A /\ B.
 // <--------------------- comment.block.coq - meta.proof.coq
 //                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ - comment.block.coq meta.proof.coq
+
+(* 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultricies laoreet faucibus. 
+// <--------------------------------------------------------------------------------------- comment.block.coq - meta.comment.hint.coq meta.comment.examples.coq
+Aenean quis vulputate libero, sit amet porta augue. Aenean vehicula non enim nec blandit.
+
+@hint Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultricies laoreet faucibus.
+// <----- comment.block.coq - meta.comment.hint.coq meta.comment.example.coq
+//    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ comment.block.coq meta.comment.hint.coq - meta.comment.examples.coq 
+Aenean quis vulputate libero, sit amet porta augue.
+// <-------------------------------------------------- meta.comment.hint.coq
+@example Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultricies laoreet faucibus.
+// <-------- comment.block.coq - meta.comment.example.coq meta.comment.hint.coq
+//        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ comment.block.coq meta.comment.example.coq  - meta.comment.hint.coq
+Aenean quis vulputate libero, sit amet porta augue.
+*)
+
+Lemma and_intro : forall A B : Prop, A -> B -> A /\ B.
+// <--- - meta.comment.examples.coq
